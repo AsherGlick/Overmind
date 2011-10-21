53751087435aa4ba7eb55c11e76b01ce714f8f70
Dave <davemezaros@gmail.com>
2011-10-21 08:04:14 -0400

    content switch fix/calender view added (broken)

--- Android_Application/ui/src/com/test/relative/TestRelativeLayoutActivity.java
+++ Android_Application/ui/src/com/test/relative/TestRelativeLayoutActivity.java
@@ -10,10 +10,26 @@ public class TestRelativeLayoutActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        /* the following is code for creating a MultiAutoCompleteTextView
+         * (basically an adaption of the auto complete text box)
+         * - custom suggestions are set in COUNTRIES, but the suggestions
+         * appear to show above the text box, while normal suggestions still
+         * appear below as usual
+         * 
+         * ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+        MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) findViewById(R.id.edit);
+        textView.setAdapter(adapter);
+        textView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());*/
+        
     }
-    public void selfDestruct(View view) {
+    public void testPage(View view) {
         // Kablam
     	setContentView(R.layout.page2_main);
     	
     }
+    /*private static final String[] COUNTRIES = new String[] {
+        "Belgium", "France", "Italy", "Germany2", "Spain"
+    };*/
 }
