e761305ad89a72d81bd212c4b54f0a2722e70c17
Victor Huang <huangv1201@gmail.com>
2011-11-29 13:59:05 -0500

    full demo directory

---
+++ Android_Application/Demo2/Demo/src/overmind/demo/Schedule.java
@@ -0,0 +1,32 @@
+package overmind.demo;
+
+public class Schedule 
+{
+	private String roomid;
+	private String timeStart;
+	private String timeEnd;
+	public Schedule(String newroomid, String newtimeStart, String newtimeEnd)
+	{
+		roomid = newroomid;
+		timeStart = newtimeStart;
+		timeEnd = newtimeEnd;
+	}
+	//time formatted as such: "0y1y2-3m4m5-6d7d8 9h10h11-12m13m14-15s16s"
+	public String getDate()
+	{
+		return timeStart.substring(0, 7);
+	}
+	public String getStartTime()
+	{
+		return timeStart.substring(9, 13);
+	}
+	public String getEndTime()
+	{
+		return timeEnd.substring(9, 13);
+	}
+	public String getRoom()
+	{
+		return roomid;
+	}
+	
+}
