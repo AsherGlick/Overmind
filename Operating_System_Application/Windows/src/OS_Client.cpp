f26c1dbd81ce9c4bb1a4224d40e388b9b68a8a13
ZiMing Lin <zi.lin9@gmail.com>
2011-12-08 23:40:19 -0500

    Managed directories.

---
+++ Operating_System_Application/Windows/src/OS_Client.cpp
@@ -0,0 +1,228 @@
+ /****\
+ * [OS_Client.cpp] is part of Project Overmind and is
+ * under the same licence
+ * Compile with -L"C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib" and -lw2_32
+ * Authors: [Zi Ming Lin]
+ \****/
+
+//Required to compile. Deals with certain SOCKET functions.
+#define _WIN32_WINNT 0x501
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "PowerPointApp.h"
+
+using namespace std;
+
+/*
+void* open_ppt(void* arg)
+{
+	char* name = (char*)arg;
+	execlp("C:/Program Files (x86)/Microsoft Office/Office12/POWERPNT.exe", "POWERPNT.exe", "/N", name, NULL);
+	return 0;
+}*/
+/*
+ * main
+ * starts the OS client on the background.
+ * Due to the length of the main function, there will be comments within the function.
+ */
+int main(void)
+{
+	//Initialization of variables.
+	char port[256];
+	
+	ifstream config;
+	config.open("config.txt");
+	char in[256] = "8080";
+	if(config.is_open() && !config.eof())
+		config >> in;
+	cout << in << endl;
+	strcpy(port, in);
+	config.close();
+	
+	while(true)
+	{
+		WSADATA wsaData;
+		//int test;
+	
+		SOCKET ListenSocket = INVALID_SOCKET;
+		SOCKET ClientSocket = INVALID_SOCKET;
+	
+		struct addrinfo *listenS = NULL;
+		struct addrinfo serverS;
+
+		int buffer_len = 4096;
+		char buffer[buffer_len];
+	
+		//Initializing Winsock
+		//test = WSAStartup(MAKEWORD(2,2), &wsaData);
+		//if(test != 0)
+		if (WSAStartup(MAKEWORD(2,2), &wsaData))
+		{
+			cerr << "WSAStartup failed" << endl;
+			return 1;
+		}
+	
+		ZeroMemory(&serverS, sizeof(serverS));
+		serverS.ai_family = AF_INET;
+		serverS.ai_socktype = SOCK_STREAM;
+		serverS.ai_protocol = IPPROTO_TCP;
+		serverS.ai_flags = AI_PASSIVE;
+		
+		//Set up the address and port.
+		//test = getaddrinfo(NULL, port, &server, &listen);
+		//if (test != 0)
+		if (getaddrinfo(NULL, port, &serverS, &listenS))
+		{
+			cerr << "getaddrinfo failed" << endl;
+			WSACleanup();
+			return 1;
+		}
+	
+	// Create a Socket to listen on
+		ListenSocket = socket(listenS->ai_family, listenS->ai_socktype, listenS->ai_protocol);
+		if (ListenSocket == INVALID_SOCKET)
+		{
+			cerr << "socket creation failed: " << WSAGetLastError() << endl;
+			freeaddrinfo(listenS);
+			WSACleanup();
+			return 1;
+		}
+		
+		// Setting up the socket
+		//test = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+		//if (test = SOCKET_ERROR)
+		if (bind(ListenSocket, listenS->ai_addr, (int)listenS->ai_addrlen))
+		{
+			cerr << "bind error: " << WSAGetLastError() << endl;
+			freeaddrinfo(listenS);
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+		freeaddrinfo(listenS);
+		
+		// Start listening for connections
+		// Currently assumes the phone is authorized to use the computer.
+		int test = listen(ListenSocket, SOMAXCONN);
+		if (test == SOCKET_ERROR)
+		{
+			cerr << "listen failed: " << WSAGetLastError() << endl;
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+	// Accept a client
+		ClientSocket = accept(ListenSocket, NULL, NULL);
+		if (ClientSocket == INVALID_SOCKET)
+		{
+			cerr << "accept failed: " << WSAGetLastError() << endl;
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+		closesocket(ListenSocket);
+			// Keep doing the commands the phone sends.
+		Application* app = new PowerPointApp();
+		char filename[buffer_len];
+		char filesz[buffer_len];
+		int received = 1;
+		do
+		{
+			received = recv(ClientSocket, buffer, buffer_len, 0);
+			if (received > 0)
+			{
+				printf("%s\n", buffer);
+				if(strncmp(buffer, "OPEN", 4)==0)
+				{
+					//Code to display powerpoints
+					//send(ClientSocket, "test.pptx", buffer_len, 0);
+					char* tx = strtok(buffer, "|");
+					tx = strtok(NULL, "|");
+					strcpy(filename, tx);
+					pid_t pid = fork();
+					//printf("%d\n", pid);
+					if (pid < 0)
+					{
+						perror( "fork failed\n");
+					}
+						else if( pid == 0)
+						{
+							execlp("C:/Program Files (x86)/Microsoft Office/Office12/POWERPNT.exe", "POWERPNT.exe", "/N", filename, NULL);
+						}
+						else
+						{
+						}
+				}
+				else if(strncmp(buffer, "FILE", 4)==0)
+				{
+					received = recv(ClientSocket, buffer, buffer_len, 0);
+					strcpy(filename, "t");
+					char* tx = strtok(buffer, "|");
+					strcat(filename, tx);
+						
+					tx = strtok(NULL, "|");
+					int filesize = atoi(tx);
+						
+					int output = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+					int cursize = 0;
+					while (cursize < filesize)
+					{
+						received = recv(ClientSocket, buffer, buffer_len, 0);
+						cursize = cursize+received;
+						write(output, buffer, received);
+					}
+					pid_t pid = fork();
+					//printf("%d\n", pid);
+					if (pid < 0)
+					{
+						perror( "fork failed\n");
+					}
+					else if( pid == 0)
+					{
+						execlp("C:/Program Files (x86)/Microsoft Office/Office12/POWERPNT.exe", "POWERPNT.exe", "/N", filename, NULL);
+					}
+					else
+					{
+					}
+				}
+				else
+				{
+					app->action(buffer);
+				}
+			}
+			else if(received == 0)
+			{
+				cout << "Connection closed." << endl;
+			}
+			else
+			{
+				cerr << "recv failed: " << WSAGetLastError() << endl;
+				closesocket(ClientSocket);
+				WSACleanup();
+				return 1;
+			}
+		}
+		while (received > 0);
+
+	// Close socket
+		if (shutdown(ClientSocket, SD_SEND) == SOCKET_ERROR)
+		{
+			cerr << "shutdown failed: " << WSAGetLastError() << endl;
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+		closesocket(ClientSocket);
+		WSACleanup();
+	}
+	return 0;
+}
---
+++ Operating_System_Application/Windows/src/PowerPointApp.h
@@ -0,0 +1,75 @@
+/*
+ * Application.h
+ * Header file for the PowerPointApp.
+ * It implements the functions as well.
+ * PowerPointApp extends Application.
+ */
+#ifndef PowerPointApp_H
+#define PowerPointApp_H
+#include <string.h>
+#include "Application.h"
+class PowerPointApp : public Application
+{
+private:
+/* 
+ * full_screen()
+ * Emulates the key 'F5' button being pushed.
+ */
+	void full_screen()
+	{
+		keybd_event(VK_F5, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+		keybd_event(VK_F5, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+	}
+	
+/*
+ * prev_slide()
+ * Emulates the key 'Left Arrow' being pushed.
+ */
+	void prev_slide()
+	{
+		keybd_event(VK_LEFT, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+		keybd_event(VK_LEFT, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);	
+	}
+
+/*
+ * next_slide()
+ * Emulates the key 'Right Arrow' being pushed.
+ */
+	void next_slide()
+	{
+		keybd_event(VK_RIGHT, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+		keybd_event(VK_RIGHT, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+	}
+/*
+ * close()
+ * Emulates the key 'ESC' being pushed.
+ */
+	void close()
+	{
+		keybd_event(VK_ESCAPE, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+		keybd_event(VK_ESCAPE, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+		keybd_event(VK_MENU, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+		keybd_event(VK_F4, 0x45, KEYEVENTF_EXTENDEDKEY | 0, 0);
+		keybd_event(VK_F4, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+		keybd_event(VK_MENU, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+	}
+	
+public:
+/*
+ * action(char* command)
+ * Takes in a string and runs the appropriate function.
+ */
+	void action(char* command)
+	{
+		if (strncmp(command, "FULL",4)==0)
+			full_screen();
+		if (strncmp(command, "NEXT",4)==0)
+			next_slide();
+		if (strncmp(command, "PREV",4)==0)
+			prev_slide();
+		if (strncmp(command, "SHUT",4)==0)
+			close();
+	}
+};
+
+#endif
